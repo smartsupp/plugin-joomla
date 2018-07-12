2a868fd7a6abd8ac8cbfc35c332cee99760609cf
Krsiak Daniel <krsiak.daniel@gmail.com>
2018-07-12 16:39:25 +0200

    Must be in folder (ZIP) for CMS manual upload

---
+++ smartsupp/src/helper.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Smartsupp Live Chat integration module.
+ * 
+ * @package   Smartsupp
+ * @author    Smartlook <vladimir@smartsupp.com>
+ * @link      http://www.smartsupp.com
+ * @copyright 2015 Smartsupp.com
+ * @license   GPL-2.0+
+ *
+ * Plugin Name:       Smartsupp
+ * Plugin URI:        http://www.smartsupp.com
+ * Description:       Adds Smartupp Live Chat code to Joomla.
+ * Version:           1.0.0
+ * Author:            Smartsupp
+ * Author URI:        http://www.smartsupp.com
+ * Text Domain:       smartlook
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+?>
+
+<?php
+defined('_JEXEC') or die('Restricted access');
